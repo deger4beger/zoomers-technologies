f2ef27c57e8d0571271c545d97a8af14b9c0430c
deger4beger <deger.begerrr@gmail.com>
2022-03-28 14:23:02 +0600

    mercurius setup

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,19 +1,17 @@
-import fastify, { FastifyInstance } from "fastify"
 import { Server, IncomingMessage, ServerResponse } from "http"
-import { ApolloServerPlugin } from "apollo-server-plugin-base"
-import { ApolloServer } from "apollo-server-fastify"
+import fastify, { FastifyInstance, FastifyRegisterOptions } from "fastify"
+import mercurius, { MercuriusOptions } from "mercurius"
 
 import { requestSerializer, responseSerializer } from "./serializers"
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core"
 import { config } from "../lib/config"
 import { generateSchema } from "./resolvers"
 
 export class FastifyCore {
 
-	private readonly fastify: FastifyInstance<Server, IncomingMessage, ServerResponse>
+	private readonly server: FastifyInstance<Server, IncomingMessage, ServerResponse>
 
 	constructor() {
-		this.fastify = fastify({
+		this.server = fastify({
 			logger: {
 				level: config.logger.level,
                 prettyPrint: config.logger.prettyPrint,
@@ -24,41 +22,26 @@ export class FastifyCore {
                 },
 			} as any
 		})
+
 	}
 
 	async listen(): Promise<unknown> {
 		try {
-			await this.startApolloServer()
-			return this.fastify.listen(config.port, config.host)
+			await this.registerGraphqlServer()
+			return this.server.listen(config.port, config.host)
 		} catch (err) {
-			this.fastify.log.error(err)
+			this.server.log.error(err)
 			process.exit(1)
 		}
 	}
 
-	private async startApolloServer(): Promise<void> {
+	private async registerGraphqlServer() {
 		const schema = await generateSchema()
-	  	const apolloServer = new ApolloServer({
-	    	schema,
-	    	plugins: [
-	      		this.fastifyAppClosePlugin(this.fastify),
-	      		ApolloServerPluginDrainHttpServer({ httpServer: this.fastify.server })
-	    	]
-	  	} as any)
-	  	await apolloServer.start()
-	  	this.fastify.register(apolloServer.createHandler())
-	}
-
-	private fastifyAppClosePlugin(app: FastifyInstance): ApolloServerPlugin {
-	  	return {
-	    	async serverWillStart() {
-	      		return {
-			        async drainServer() {
-			          	await app.close()
-			        }
-			    }
-	   		}
-	  	}
+		const opts: FastifyRegisterOptions<MercuriusOptions> = {
+			schema,
+			graphiql: true
+		}
+		this.server.register(mercurius, opts)
 	}
 
 }
